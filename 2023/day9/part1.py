fb6bb0c6ed2db0dd49ad2ad8d4bbb9037b4b68da
Benson Kalahar <bensonk42@gmail.com>
2023-12-08 21:36:54 -0800

    Part 1 day 9

---
+++ 2023/day9/part1.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+from pprint import pprint
+import functools, re, sys, unittest
+
+def parse(line):
+  return [int(x) for x in line.split()]
+
+def all_zeros(seq):
+  for x in seq:
+    if x != 0:
+      return False
+  return True
+
+def find_next(seq, depth=0):
+  diffs = [b - a for a, b in zip(seq, seq[1:])]
+  if all_zeros(diffs):
+    return seq[-1]
+  else:
+    return seq[-1] + find_next(diffs)
+
+
+def main(files):
+  for fname in files:
+    with open(fname) as f:
+      lines = list(map(parse, f))
+
+    nexts = [find_next(l) for l in lines]
+    print(nexts)
+    print(f'Sum: {sum(nexts)}')
+
+class MapsTest(unittest.TestCase):
+  def setUp(self):
+    pass
+
+  def test_stuff(self):
+    pass
+
+if __name__ == '__main__':
+  main(sys.argv[1:])
