f90b560716992e7fbc33f1072d8b13b38a23e2b3
Benson Kalahar <bensonk42@gmail.com>
2023-12-10 03:10:01 -0800

    updates grid helper

--- 2023/helpers/grid.py
+++ 2023/helpers/grid.py
@@ -9,6 +9,10 @@ class Grid(object):
     self.width = max(len(l) for l in self.grid)
 
   def mark(self, color, predicate):
+    if type(predicate) == tuple:
+      self.set_meta(predicate, 'color', color)
+      return
+
     for x in range(self.width):
       for y in range(self.height):
         coord = (x, y)
@@ -16,20 +20,59 @@ class Grid(object):
         if predicate(coord, point['val']):
           point['color'] = color
 
+  def flood_fill(self, color, start):
+    start_d = self._get(start)
+    prev_color = start_d['color'] if 'color' in start_d else None
+    start_d['color'] = color
+
+    coords = [c for c in (self.up(start), self.down(start), self.left(start), self.right(start)) if c]
+    for point in coords:
+      target = self._get(point)
+      if 'color' in target:
+        if (target['color'] == prev_color):
+          self.flood_fill(color, point)
+      elif (color not in target) and prev_color == None:
+        self.flood_fill(color, point)
+
+  def find(self, c):
+    positions = []
+    for y_inverse, line in enumerate(self.grid):
+      for x, point in enumerate(line):
+        if point['val'] == c:
+          y = self.height - (y_inverse+1)
+          positions.append((x, y))
+    return positions
+
   def get_meta(self, point, key):
-    return self.grid[point[1]][point[0]]
+    return self._get(point)[key]
 
   def set_meta(self, point, key, val):
-    self.grid[point[1]][point[0]] = val
+    self._get(point)[key] = val
 
   def _get(self, point):
-    return self.grid[point[1]][point[0]]
+    return self.grid[self.height-point[1]-1][point[0]]
 
   def get(self, point):
-    return self.grid[point[1]][point[0]]['val']
+    return self._get(point)['val']
 
   def set(self, point, val):
-    self.grid[point[1]][point[0]]['val'] = val
+    self._get(point)['val'] = val
+
+  def up(self, point, dist=1):
+    if point[1] <= (self.height - dist):
+      return point[0], point[1]+dist
+
+  def down(self, point, dist=1):
+    if point[1] > 0:
+      return point[0], point[1]-dist
+
+  def left(self, point, dist=1):
+    if point[0] > 0:
+      return point[0]-dist, point[1]
+
+  def right(self, point, dist=1):
+    if point[0] <= (self.width - dist - 1):
+      return point[0]+dist, point[1]
 
   def __str__(self):
     s = ''
@@ -56,11 +99,12 @@ def main(args):
     with open(fname) as f:
       g = Grid(f)
       g.mark('green', lambda _, v: v == '*')
-      g.mark('red', lambda _, v: v == '#')
-      g.mark('blue', lambda _, v: v == '%')
-      g.mark('yellow', lambda _, v: v == '/')
+      print(g)
+      g.flood_fill('yellow', (0,0))
+      g.flood_fill('blue', (32, 32))
       print(g)
 
 if __name__ == '__main__':
   import sys
+  sys.setrecursionlimit(40000)
   main(sys.argv[1:])
